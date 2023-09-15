745c1d7c6e48d3d3b86d98c09a7afe03ee7ef60f
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2023-09-15 09:06:50 +0300

    muutin Main.java

--- Harkka1.java
+++ Harkka1.java
@@ -1,6 +1,6 @@
 public class Main {    
     public static void main(String [] args) {
-
+        // sdkjdsljkjldsklkj
     }
     public static void print(Object o) {
         System.out.println(o);   
